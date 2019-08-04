36b1f4c37e40d6ff88503c9484b012c0b7c3cd63
fwchen <me@chenfangwei.com>
2019-08-04 23:55:23 +0800

    initial commit

---
+++ src/main/java/com/chenfangwei/tnki/TnkiApplication.java
@@ -0,0 +1,13 @@
+package com.chenfangwei.tnki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TnkiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TnkiApplication.class, args);
+    }
+
+}
